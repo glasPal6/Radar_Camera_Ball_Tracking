58f9c7fa74bd53a2221e501785da4dbdb7709c2a
glaspal6 <dylan.kamstra@protonmail.com>
2024-03-11 15:14:01 +0200

    Refactored the code to plot the image

--- src/Radar_Calibration/radar_calibration.py
+++ src/Radar_Calibration/radar_calibration.py
@@ -65,6 +65,70 @@ def radar_Kalman(targets, measurements, noise_m, noise_p=0):
     t = A[:2, 2]
     return A, R, t
 
+def plot_calibration_image(config, azimuth_data):
+    # Extract config
+    tx_azimuth_antennas = config['Azimuth antennas']
+    rx_antennas = config['Receive antennas']
+    range_bins = config['Range bins'] 
+    angle_bins = config['Doppler bins']
+    range_res = config['Range resolution (m)'] 
+    range_bias = config['Range Bias (m)'] if "Range Bias (m)" in config.keys() else 0
+    
+    # define the space
+    t = np.array(range(-angle_bins//2 + 1, angle_bins//2)) * (2 / angle_bins)
+    t = np.arcsin(t)
+    r = np.array(range(range_bins)) * range_res
+
+    range_depth = range_bins * range_res
+    range_width, grid_res = range_depth / 2, 400
+
+    xi = np.linspace(-range_width, range_width, grid_res)
+    yi = np.linspace(0, range_depth, grid_res)
+    xi, yi = np.meshgrid(xi, yi)
+
+    x = np.array([r]).T * np.sin(t)
+    y = np.array([r]).T * np.cos(t)
+    y = y - range_bias
+
+    # Create the figure
+    fig = plt.figure(figsize=(6, 6))
+    ax = plt.subplot(1, 1, 1)  # rows, cols, idx
+    fig.tight_layout()
+    cm = ax.imshow(((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, extent=[-range_width, +range_width, 0, range_depth], alpha=0.95)
+                       
+    ax.set_title('Azimuth-Range FFT Heatmap [{};{}]'.format(angle_bins, range_bins), fontsize=10)
+    ax.set_xlabel('Lateral distance along [m]')
+    ax.set_ylabel('Longitudinal distance along [m]')
+    
+    ax.plot([0, 0], [0, range_depth], color='white', linewidth=0.5, linestyle=':', zorder=1)
+    ax.plot([0, -range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)
+    ax.plot([0, +range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)    
+
+    ax.set_ylim([0, +range_depth])
+    ax.set_xlim([-range_width, +range_width])
+
+    # draw 45 degree lines
+    for i in range(1, int(range_depth)+1):
+        ax.add_patch(pat.Arc((0, 0), width=i*2, height=i*2, angle=90, theta1=-90, theta2=90, color='white', linewidth=0.5, linestyle=':', zorder=1))
+
+    # Plot the data
+    a = azimuth_data
+    a = np.array([a[i] + 1j * a[i+1] for i in range(0, len(a), 2)])
+    a = np.reshape(a, (range_bins, tx_azimuth_antennas * rx_antennas))
+    a = np.fft.fft(a, angle_bins)
+    a = np.abs(a)
+    a = np.fft.fftshift(a, axes=(1,))  # put left to center, put center to right       
+    a = a[:,1:]  # cut off first angle bin
+      
+    # Interpolate the data
+    zi = spi.griddata((x.ravel(), y.ravel()), a.ravel(), (xi, yi), method='linear')
+    zi = zi[:-1,:-1]
+      
+    cm.set_array(zi[::-1,::-1])  # rotate 180 degrees
+    cm.autoscale()
+
+    plt.show()
+
 def data_extraction(data_path, gt_positions_path, config_path):
     """
     This assumes satic positions of the corner reflectors
@@ -84,76 +148,8 @@ def data_extraction(data_path, gt_positions_path, config_path):
     with open(config_path, 'r') as f:
         config = json.load(f)
 
-    tx_azimuth_antennas = config['Azimuth antennas']
-    rx_antennas = config['Receive antennas']
-    range_bins = config['Range bins'] 
-    angle_bins = config['Doppler bins']
-    range_res = config['Range resolution (m)'] 
-    range_bias = config['Range Bias (m)'] if "Range Bias (m)" in config.keys() else 0
+    plot_calibration_image(config, data[0]['dataFrame']['azimuth_static'])
 
-    t = np.array(range(-angle_bins//2 + 1, angle_bins//2)) * (2 / angle_bins)
-    t = np.arcsin(t) # t * ((1 + np.sqrt(5)) / 2)
-    r = np.array(range(range_bins)) * range_res
-
-    range_depth = range_bins * range_res
-    range_width, grid_res = range_depth / 2, 400
-    
-    xi = np.linspace(-range_width, range_width, grid_res)
-    yi = np.linspace(0, range_depth, grid_res)
-    xi, yi = np.meshgrid(xi, yi)
-
-    x = np.array([r]).T * np.sin(t)
-    y = np.array([r]).T * np.cos(t)
-    y = y - range_bias
-
-    # ---
-
-    fig = plt.figure(figsize=(6, 6))
-    ax = plt.subplot(1, 1, 1)  # rows, cols, idx
-    
-    fig.tight_layout(pad=2)
-    
-    #n = range_bins
-    #z = np.array(([0, 1] * n + [1, 0] * n) * n)
-    #z.shape = (n*2, n*2)
-    #im = ax.imshow(z, cmap=plt.cm.gray, interpolation='nearest', extent=[-range_width, +range_width, 0, range_depth])
-
-    cm = ax.imshow(((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, extent=[-range_width, +range_width, 0, range_depth], alpha=0.95)
-    #cm = ax.pcolormesh(xi, yi, ((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, alpha=0.95)
-    
-    fig.canvas.manager.set_window_title('...')
-                       
-    ax.set_title('Azimuth-Range FFT Heatmap [{};{}]'.format(angle_bins, range_bins), fontsize=10)
-    ax.set_xlabel('Lateral distance along [m]')
-    ax.set_ylabel('Longitudinal distance along [m]')
-    
-    ax.plot([0, 0], [0, range_depth], color='white', linewidth=0.5, linestyle=':', zorder=1)
-    ax.plot([0, -range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)
-    ax.plot([0, +range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)    
-
-    ax.set_ylim([0, +range_depth])
-    ax.set_xlim([-range_width, +range_width])
-
-    for i in range(1, int(range_depth)+1):
-        ax.add_patch(pat.Arc((0, 0), width=i*2, height=i*2, angle=90, theta1=-90, theta2=90, color='white', linewidth=0.5, linestyle=':', zorder=1))
-
-    a = data[0]['dataFrame']['azimuth_static']
-
-    a = np.array([a[i] + 1j * a[i+1] for i in range(0, len(a), 2)])
-    a = np.reshape(a, (range_bins, tx_azimuth_antennas * rx_antennas))
-    a = np.fft.fft(a, angle_bins)
-     
-    a = np.abs(a)
-    a = np.fft.fftshift(a, axes=(1,))  # put left to center, put center to right       
-    a = a[:,1:]  # cut off first angle bin
-      
-    zi = spi.griddata((x.ravel(), y.ravel()), a.ravel(), (xi, yi), method='linear')
-    zi = zi[:-1,:-1]
-      
-    cm.set_array(zi[::-1,::-1])  # rotate 180 degrees
-    cm.autoscale()
-
-    plt.show()
     exit() 
 
     # Load the ground truth positions
