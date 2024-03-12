d11de28bf2836a61f9edbe8a4f28bedc2df2d859
glaspal6 <dylan.kamstra@protonmail.com>
2024-03-12 10:58:26 +0200

    Got a rough max in the rectangle

--- src/Radar_Calibration/radar_calibration.py
+++ src/Radar_Calibration/radar_calibration.py
@@ -101,68 +101,42 @@ def extract_azimuth_data(config, azimuth_data):
     zi = spi.griddata((x.ravel(), y.ravel()), a.ravel(), (xi, yi), method='linear')
     zi = zi[:-1,:-1]
 
-    return zi
+    return zi[::-1, ::-1]
 
-def plot_calibration_image(config, azimuth_data, reflector_coordinates_path):
+def plot_calibration_image(config, azimuth_data):
     # Extract config
-    tx_azimuth_antennas = config['Azimuth antennas']
-    rx_antennas = config['Receive antennas']
     range_bins = config['Range bins'] 
     angle_bins = config['Doppler bins']
     range_res = config['Range resolution (m)'] 
-    range_bias = config['Range Bias (m)'] if "Range Bias (m)" in config.keys() else 0
     
-    # define the space
-    t = np.array(range(-angle_bins//2 + 1, angle_bins//2)) * (2 / angle_bins)
-    t = np.arcsin(t)
-    r = np.array(range(range_bins)) * range_res
-
     range_depth = range_bins * range_res
-    range_width, grid_res = range_depth / 2, 400
-
-    xi = np.linspace(-range_width, range_width, grid_res)
-    yi = np.linspace(0, range_depth, grid_res)
-    xi, yi = np.meshgrid(xi, yi)
-
-    x = np.array([r]).T * np.sin(t)
-    y = np.array([r]).T * np.cos(t)
-    y = y - range_bias
+    range_width, grid_res = range_depth / 2, 400 -1
 
     # Create the figure
     fig = plt.figure(figsize=(6, 6))
     ax = plt.subplot(1, 1, 1)  # rows, cols, idx
     fig.tight_layout()
-    cm = ax.imshow(((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, extent=[-range_width, +range_width, 0, range_depth], alpha=0.95)
+    #cm = ax.imshow(((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, extent=[-range_width, +range_width, 0, range_depth], alpha=0.95)
+    cm = ax.imshow(((0,)*grid_res,) * grid_res, cmap=plt.cm.jet, alpha=0.95)
                        
     ax.set_title('Azimuth-Range FFT Heatmap [{};{}]'.format(angle_bins, range_bins), fontsize=10)
     ax.set_xlabel('Lateral distance along [m]')
     ax.set_ylabel('Longitudinal distance along [m]')
     
-    ax.plot([0, 0], [0, range_depth], color='white', linewidth=0.5, linestyle=':', zorder=1)
-    ax.plot([0, -range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)
-    ax.plot([0, +range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)    
+    #ax.plot([0, 0], [0, range_depth], color='white', linewidth=0.5, linestyle=':', zorder=1)
+    #ax.plot([0, -range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)
+    #ax.plot([0, +range_width], [0, range_width], color='white', linewidth=0.5, linestyle=':', zorder=1)    
 
-    ax.set_ylim([0, +range_depth])
-    ax.set_xlim([-range_width, +range_width])
+    #ax.set_ylim([0, +range_depth])
+    #ax.set_xlim([-range_width, +range_width])
 
     # draw 45 degree lines
     for i in range(1, int(range_depth)+1):
         ax.add_patch(pat.Arc((0, 0), width=i*2, height=i*2, angle=90, theta1=-90, theta2=90, color='white', linewidth=0.5, linestyle=':', zorder=1))
 
     # Plot the data
-    a = azimuth_data
-    a = np.array([a[i] + 1j * a[i+1] for i in range(0, len(a), 2)])
-    a = np.reshape(a, (range_bins, tx_azimuth_antennas * rx_antennas))
-    a = np.fft.fft(a, angle_bins)
-    a = np.abs(a)
-    a = np.fft.fftshift(a, axes=(1,))  # put left to center, put center to right       
-    a = a[:,1:]  # cut off first angle bin
-      
-    # Interpolate the data
-    zi = spi.griddata((x.ravel(), y.ravel()), a.ravel(), (xi, yi), method='linear')
-    zi = zi[:-1,:-1]
-      
-    cm.set_array(zi[::-1,::-1])  # rotate 180 degrees
+    data = extract_azimuth_data(config, azimuth_data)
+    cm.set_array(data)  # rotate 180 degrees
     cm.autoscale()
 
     return fig, ax, cm
@@ -187,7 +161,14 @@ def get_radar_points(config, azimuth_data, reflector_coordinates_path):
     with open(reflector_coordinates_path, 'w') as f:
         f.write("")
 
-    fig, ax, cm = plot_calibration_image(config, azimuth_data, reflector_coordinates_path)
+    # Extract config
+    range_bins = config['Range bins'] 
+    range_res = config['Range resolution (m)'] 
+    
+    range_depth = range_bins * range_res
+    range_width, grid_res = range_depth / 2, 400
+
+    fig, ax, cm = plot_calibration_image(config, azimuth_data)
     rs = RectangleSelector(ax, line_select_callback,
                        useblit=True,
                        button=[1, 3],  # don't use middle button
@@ -196,12 +177,32 @@ def get_radar_points(config, azimuth_data, reflector_coordinates_path):
                        interactive=True)
     plt.show()
 
-def get_maximun_points(azimuth_data, reflector_coordinates_path):
+def get_maximun_points(config, azimuth_data, reflector_coordinates_path):
     """
     Get the maximum points
     """
-    cnr_ref = np.loadtxt(reflector_coordinates_path, delimiter=",")
-    print(cnr_ref)
+    # Extract and group the points
+    cnr_ref = np.loadtxt(reflector_coordinates_path, delimiter=",").reshape((-1, 2, 2))
+    data = extract_azimuth_data(config, azimuth_data)
+    print(np.max(data))
+    print()
+
+    fig, ax, cm = plot_calibration_image(config, azimuth_data)
+    # Find the maximun
+    for sqr in cnr_ref:
+        x1, x2 = int(np.round(sqr[0, 0])), int(np.round(sqr[1, 0]))
+        y1, y2 = int(np.round(sqr[0, 1])), int(np.round(sqr[1, 1]))
+        max = np.unravel_index(np.argmax(data[y1:y2, x1:x2]), data[y1:y2, x1:x2].shape)
+        ax.scatter([x1,x2,x1+max[1]], [y1,y2,y1+max[0]])
+        print(data[y1, x1])
+        print(data[y2, x2])
+        print(np.max(data[y1:y2, x1:x2]))
+        print(data[y1:y2, x1:x2].shape)
+        print(np.unravel_index(np.argmax(data[y1:y2, x1:x2]), data[y1:y2, x1:x2].shape))
+        print(data[max[0]+y1, max[1]+x1] == np.max(data[y1:y2, x1:x2]))
+        print()
+    
+    plt.show()
 
 def show_maximum_points(azimuth_data, reflector_coordinates_path):
     """
@@ -230,8 +231,8 @@ def data_extraction(data_path, gt_positions_path, config_path, reflector_coordin
         config = json.load(f)
 
     # Write the calibration points to a file
-    get_radar_points(config, data[0]['dataFrame']['azimuth_static'], reflector_coordinates_path)
-    max_points = get_maximun_points(data[0]['dataFrame']['azimuth_static'], reflector_coordinates_path)
+    # get_radar_points(config, np.array(data[0]['dataFrame']['azimuth_static']), reflector_coordinates_path)
+    max_points = get_maximun_points(config, np.array(data[0]['dataFrame']['azimuth_static']), reflector_coordinates_path)
 
     exit() 
 
